95a105799214ce88fccd0ac9c383a9d19750a26c
gitsobek <sobus.piotr@gmail.com>
2017-11-21 22:30:42 +0100

    Add action links

--- components/ActionLinks.vue
+++ components/ActionLinks.vue
@@ -1,16 +1,75 @@
 <template>
   <section class="action-links">
-    Action Links
+    <action-link-item v-for="(link, index) in links" :key ="index"
+      :class="['action-links__link-item', link.itemClass]"
+      :linktext="link.text"
+      :iconClass="link.icon">
+    </action-link-item>
   </section>
 </template>
+
+<script>
+import ActionLinkItem from '~/components/partials/ActionLinkItem'
+
+export default {
+  data () {
+    return {
+      links: [
+        {
+          text: 'Dodaj nową ofertę pracy!',
+          icon: 'flaticon-job-search',
+          itemClass: 'action-link--job-offer'
+        },
+        {
+          text: 'Dodaj nowy projekt i znajdź fantastyczny zespół',
+          icon: 'flaticon-teamwork',
+          itemClass: 'action-link--project-offer'
+        },
+        {
+          text: 'Dowiedź się więcej o KNIT PK',
+          icon: 'flaticon-info',
+          itemClass: 'action-link--more-info'
+        },
+        {
+          text: 'Utwórz sekcję i wspólnie poszerzaj swoją wiedzę',
+          icon: 'flaticon-programming',
+          itemClass: 'action-link--new-section'
+        }
+      ]
+    }
+  },
+  components: {
+    ActionLinkItem
+  }
+}
+</script>
 
 <style lang="scss">
 @import "assets/scss/_imports.scss";
 
 .action-links {
+  display: flex;
+  flex-wrap: wrap;
   flex-basis: calc(50% - #{$default-gutters-width / 2});
   margin-left: $default-gutters-width / 2;
-  background-color: #fff;
+  background-color: $gray-20;
+  justify-content: space-between;
+  align-content: space-between;
+  overflow: hidden;
+  position: relative;
+  font-size: 0.875rem;
+
+  &__link-item {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    flex-basis: calc(50% - #{$default-gutters-width / 2});
+    border-radius: 5px;
+    max-width: 200px;
+    max-height: 150px;
+    min-width: calc(50% - #{$default-gutters-width / 2});
+    min-height: calc(50% - #{$default-gutters-width / 2});
+    padding: 25px;
+  }
 }
 </style>
-
---
+++ components/partials/ActionLinkItem.vue
@@ -0,0 +1,73 @@
+<template>
+  <a class="action-link" href="#">
+    <span class="action-link__text">{{ linktext }}</span>
+    <span :class="[iconClass]" class="action-link__icon"></span>
+  </a>
+</template>
+
+<script>
+export default {
+  props: {
+    linktext: {
+      type: String,
+      default: 'empty'
+    },
+    iconClass: {
+      type: String
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import "assets/scss/_imports.scss";
+
+.action-link {
+  $p: &;
+
+  &__text {
+    color: $action-link-item-text;
+    text-transform: uppercase;
+    text-decoration: none;
+  }
+
+  &__icon {
+    &:before {
+      font-size: 3rem;
+    }
+  }
+
+  &--job-offer {
+    background-color: $action-link-item-job-bgcolor;
+    #{$p}__icon {
+      color: $action-link-item-job-icon-color;
+      align-self: flex-start;
+    }
+  }
+
+  &--project-offer {
+    background-color: $action-link-item-project-bgcolor;
+    #{$p}__icon {
+      color: $action-link-item-project-icon-color;
+      align-self: flex-end;
+    }
+  }
+
+  &--more-info {
+    background-color: $action-link-item-info-bgcolor;
+    #{$p}__icon {
+      color: $action-link-item-info-icon-color;
+      align-self: flex-start;
+    }
+  }
+
+  &--new-section {
+    background-color: $action-link-item-section-bgcolor;
+    #{$p}__icon {
+      color: $action-link-item-section-icon-color;
+      align-self: flex-end;
+    }
+  }
+}
+
+</style>
