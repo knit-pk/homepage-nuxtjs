7ea7a91fcc15332e93f87140cf8ed815ab7c6188
k911 <konradobal@gmail.com>
2017-12-10 13:07:53 +0100

    Change automatic deploy branch to develop

--- deploy/mydevil.sh
+++ deploy/mydevil.sh
@@ -2,4 +2,4 @@
 
 cp deploy/mydevil-run.js app.js
 cp .env.example .env
-rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/ k911-main@s11.mydevil.net:/home/k911-main/domains/knit-test-homepage.tk/public_nodejs_ci
+rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/ k911-main@s11.mydevil.net:/home/k911-main/domains/knit-test-homepage.tk/public_nodejs
