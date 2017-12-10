dbf111199222e5617525fbdee0218d4024502697
k911 <konradobal@gmail.com>
2017-12-10 12:41:56 +0100

    Add auto-deploy to mydevil#develop

--- deploy/mydevil.sh
+++ deploy/mydevil.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
-npm8 install
-cp deploy/app.js app.js
-cp .env.mydevil .env
+cp deploy/mydevil-run.js app.js
+cp .env.example .env
+rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/ k911-main@s11.mydevil.net:/home/k911-main/domains/knit-test-homepage.tk/public_nodejs_ci
