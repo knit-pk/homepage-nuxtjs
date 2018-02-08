6f450843b226d0bb8e35adbd229c4feb8720983c
FieryCod <kwcharllie379@gmail.com>
2018-02-08 19:27:37 +0100

    Add proper scroll handler for fixedTop mixin

--- components/mixins/FixedTop.js
+++ components/mixins/FixedTop.js
@@ -3,31 +3,42 @@ import _ from 'lodash'
 export default {
   data () {
     return {
+      initializeHideHeight: 100,
       hiddenClass: 'fixed-top--hidden',
       defaultDebounceTime: 200,
       prevScrollY: 0,
-      fixedTop: null
+      fixedTop: null,
+      currScrollY: 0
+    }
+  },
+
+  computed: {
+    isScrollingDown () {
+      return this.currScrollY > this.prevScrollY
+    },
+    isScrollingUp () {
+      return this.currScrollY < this.prevScrollY
     }
   },
 
   methods: {
     fixedTopFall: _.debounce(function () {
-      this.isScrolledDown() ?
-        this.hideFixedTop() :
-        this.unhideFixedTop()
+      this.currScrollY = window.scrollY
+
+      if ((this.currScrollY > this.initializeHideHeight) && this.isScrollingDown) {
+        this.hideFixedTop()
+      } else if (this.isScrollingUp) {
+        this.showFixedTop()
+      }
 
       this.prevScrollY = window.scrollY
     }, this.defaultDebounceTime),
-
-    isScrolledDown () {
-      return window.scrollY > this.prevScrollY
-    },
 
     hideFixedTop () {
       this.fixedTop.classList.add(this.hiddenClass)
     },
 
-    unhideFixedTop () {
+    showFixedTop () {
       this.fixedTop.classList.remove(this.hiddenClass)
     }
   },
