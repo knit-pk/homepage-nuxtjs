efc1d0c971689d248c6189b4dddc8b2a0065391f
k911 <konradobal@gmail.com>
2018-03-06 21:09:36 +0100

    Configure auto-tagging on master

--- deploy/docker-app-push.sh
+++ deploy/docker-app-push.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env sh
 
 DOCKER_TAG=$(echo ${TRAVIS_TAG} | sed -E 's~^v(.*)~\1~')
 
@@ -9,5 +9,5 @@ docker push knitpk/homepage:${DOCKER_TAG}
 
 # Heroku (UAT environment)
 docker login --username=_ --password=${HEROKU_AUTH_TOKEN} registry.heroku.com
-docker tag knitpk/homepage:${DOCKER_TAG} registry.heroku.com/knit-stage-homepage/web
+docker build . -t registry.heroku.com/knit-stage-homepage/web --build-args API_URL=https://knit-test-api.tk
 docker push registry.heroku.com/knit-stage-homepage/web
