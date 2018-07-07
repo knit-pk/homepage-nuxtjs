0e76cb62d6b77b35d77f52b2921b130baece6a1e
k911 <konradobal@gmail.com>
2018-07-07 17:53:49 +0200

    ci(release): Configure automatic GitHub release
    Release process:
    1. Commit to master
    2. Create changelog using conventional changelog with angular preset
    3. Create release commit
    4. Tag release
    5. Push commit and tags to GitHub
    6. Create GitHub release

--- deploy/github-tag.sh
+++ deploy/github-tag.sh
@@ -1,6 +1,21 @@
 #!/usr/bin/env sh
 
-git tag ${APP_VERSION} --force
+if [[ $(git name-rev --name-only --tags HEAD) = "$APP_VERSION" ]]; then
+  echo "Already tagged. Skipping.."
+  exit
+fi
+
+conventional-changelog -p angular -i CHANGELOG.md -s -r 0
+git add CHANGELOG.md
+git commit -m "chore(release): ${APP_VERSION} :tada:
+
+$(conventional-changelog)
+"
+
+git tag ${APP_VERSION}
 
 git remote add authorized https://travis:${GH_TOKEN}@github.com/knit-pk/homepage-nuxtjs.git
-git push authorized ${APP_VERSION} --force
+git push authorized HEAD:master --tags
+git push authorized HEAD:develop
+
+conventional-github-releaser -p angular -t ${GH_TOKEN}
