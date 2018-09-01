dc3e3eeef2c8a91676f7e900b4fed062481c84df
k911 <konradobal@gmail.com>
2018-09-01 23:43:04 +0200

    build(mydevil): Use yarn on mydevil to install packages
    Mydevil environment is using now node 10

--- deploy/mydevil-deploy.sh
+++ deploy/mydevil-deploy.sh
@@ -2,4 +2,4 @@
 
 cp deploy/mydevil-run.js app.js
 cp .env.example .env
-rsync -r --delete-after --exclude 'node_modules' --quiet $TRAVIS_BUILD_DIR/ k911-main@s11.mydevil.net:/home/k911-main/domains/knit-test-homepage.tk/public_nodejs
+rsync -r --delete-after --exclude 'node_modules' --quiet $TRAVIS_BUILD_DIR/ k911-main@s11.mydevil.net:domains/knit-test-homepage.tk/public_nodejs
