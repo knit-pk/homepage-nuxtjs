4245e7103130c75374075e9dba2ede06c5eacad7
k911 <konradobal@gmail.com>
2018-09-02 00:06:41 +0200

    build(mydevil): Fix deploy

--- deploy/mydevil-post-deploy.sh
+++ deploy/mydevil-post-deploy.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env sh
 
+export PATH=~/.npm-global/bin:~/bin:$PATH
 cd ~/domains/knit-test-homepage.tk/public_nodejs
-rm -rf node_modules/*
+rm -rf node_modules
 yarn install
 devil www restart knit-test-homepage.tk
 mkdir public
