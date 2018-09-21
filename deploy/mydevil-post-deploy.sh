be87e142232dbd829bbd96739a95a63e7ac5fc5d
k911 <konradobal@gmail.com>
2018-09-21 21:57:07 +0200

    build(mydevil): Fix deployment
    Use nuxt-start script instead of custom one, by symlinking it to app.js

--- deploy/mydevil-post-deploy.sh
+++ deploy/mydevil-post-deploy.sh
@@ -4,7 +4,9 @@ export PATH=~/.npm-global/bin:~/bin:$PATH
 cd ~/domains/knit-test-homepage.tk/public_nodejs
 rm -rf node_modules
 yarn install
-devil www restart knit-test-homepage.tk
 mkdir public
 cp -R src/static/* public
 ln -s ../.nuxt/dist/client public/_nuxt
+yarn build
+ln -s ./node_modules/nuxt/bin/nuxt-start ./app.js
+devil www restart knit-test-homepage.tk
